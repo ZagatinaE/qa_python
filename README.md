da5a6caeb6810d3b988787d36c3a91ca70a2a4be
Ekaterina Zagatina <zagatina_e@flowwow.com>
2025-03-23 22:53:37 +0300

    Tests for BooksCollector

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # qa_python
+# 1.test_add_new_book_add_only_once
+# 2.test_add_new_book_valid_name
+# 3. test_add_new_book_empty_name
+# 4. test_add_new_book_longer_40_name
+# 5. test_set_book_genre_and_get_book_genre
+# 6. test_get_books_with_specific_genre
+# 7. test_get_books_for_children
+# 8. test_add_book_in_favorites
+# 9.test_delete_book_from_favorites
+# 10.test_get_list_of_favorites_books
+# 11.test_get_books_genre
+# 12.test_set_book_genre_unknown_genre
+# 13.test_add_book_in_favorites_not_in_collection
--- tests.py
+++ tests.py
@@ -1,24 +1,99 @@
+import pytest
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
-class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+@pytest.fixture(scope="function")
+def collector():
+    return BooksCollector()
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+def test_add_new_book_add_only_once(collector):
+    collector.add_new_book('Предубеждение и предубеждение')
+    collector.add_new_book('Предубеждение и предубеждение')
+    assert len(collector.get_books_genre()) == 1
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+def test_add_new_book_valid_name(collector):
+    collector.add_new_book('Гордость и предубеждение')
+    assert 'Гордость и предубеждение' in collector.get_books_genre()
+
+
+def test_add_new_book_empty_name(collector):
+    collector.add_new_book('')
+    assert '' not in collector.get_books_genre()
+
+
+def test_add_new_book_longer_40_name(collector):
+    collector.add_new_book('Гордость и предубеждение и Предубеждение и Гордость')
+    assert 'Гордость и предубеждение и Предубеждение и Гордость' not in collector.get_books_genre()
+
+
+def test_set_book_genre_and_get_book_genre(collector):
+    collector.add_new_book('Гордость и предубеждение - 2')
+    collector.set_book_genre('Гордость и предубеждение - 2', 'Комедии')
+    assert collector.get_book_genre('Гордость и предубеждение - 2') == 'Комедии'
+
+
+@pytest.mark.parametrize('book_genre,save_books', [('Фантастика', ['Гордость']),
+    ('Ужасы', ['Предубеждение']),
+    ('Детективы', ['Дневник Бриджит Джонс']),
+])
+def test_get_books_with_specific_genre(collector, book_genre,save_books):
+    collector.add_new_book('Гордость')
+    collector.set_book_genre('Гордость', 'Фантастика')
+    collector.add_new_book('Предубеждение')
+    collector.set_book_genre('Предубеждение', 'Ужасы')
+    collector.add_new_book('Дневник Бриджит Джонс')
+    collector.set_book_genre('Дневник Бриджит Джонс', 'Детективы')
+    assert collector.get_books_with_specific_genre(book_genre) == save_books
+
+
+def test_get_books_for_children(collector):
+    collector.add_new_book('Гордость')
+    collector.set_book_genre('Гордость', 'Фантастика')
+    collector.add_new_book('Предубеждение')
+    collector.set_book_genre('Предубеждение', 'Ужасы')
+    collector.add_new_book('Дневник Бриджит Джонс')
+    collector.set_book_genre('Дневник Бриджит Джонс', 'Детективы')
+    collector.add_new_book('Незнайка на луне')
+    collector.set_book_genre('Незнайка на луне', 'Мультфильмы')
+    collector.add_new_book('12 стульев')
+    collector.set_book_genre('12 стульев', 'Комедии')
+    assert collector.get_books_for_children() == ['Гордость', 'Незнайка на луне', '12 стульев']
+
+
+def test_add_book_in_favorites(collector):
+    collector.add_new_book('Дневник Бриджит Джонс 2')
+    collector.add_book_in_favorites('Дневник Бриджит Джонс 2')
+    assert 'Дневник Бриджит Джонс 2' in collector.get_list_of_favorites_books()
+
+
+def test_delete_book_from_favorites(collector):
+    collector.add_new_book('Дневник Бриджит Джонс 2')
+    collector.add_book_in_favorites('Дневник Бриджит Джонс 2')
+    collector.delete_book_from_favorites('Дневник Бриджит Джонс 2')
+    assert 'Дневник Бриджит Джонс 2' not in collector.get_list_of_favorites_books()
+
+
+def test_get_list_of_favorites_books(collector):
+    collector.add_new_book('Дневник Бриджит Джонс 3')
+    collector.add_book_in_favorites('Дневник Бриджит Джонс 3')
+    assert collector.get_list_of_favorites_books() == ['Дневник Бриджит Джонс 3']
+
+
+def test_get_books_genre(collector):
+    collector.add_new_book('Незнайка на луне')
+    collector.set_book_genre('Незнайка на луне', 'Мультфильмы')
+    assert collector.get_books_genre() == {'Незнайка на луне': 'Мультфильмы'}
+
+
+def test_set_book_genre_unknown_genre(collector):
+    collector.add_new_book('Страна Оз')
+    collector.set_book_genre('Страна Оз', 'Криминал')
+    assert collector.get_book_genre('Страна Оз') not in collector.genre
+    assert collector.get_book_genre('Страна Оз') == ''
+
+
+def test_add_book_in_favorites_not_in_collection(collector):
+    collector.add_book_in_favorites('Капитанская дочка')
+    assert 'Капитанская дочка' not in collector.get_list_of_favorites_books()
